08221bb2e1cd83d6b40416895403eca35b1643a8
Owain Kenway <o.kenway@ucl.ac.uk>
2023-03-16 13:51:18 +0000

    Script postprocessing.

---
+++ version_study/post-process.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source /etc/profile.d/modules.sh
+
+module load spack-test
+module load vmd/1.9.3/text-only
+module load imagemagick-7.0.8-7-gcc-12.2.0-zj7gknr
+
+vmd -dispdev text -eofexit -e render.tcl
+
+for a in *.rgb
+do
+	convert -strip $a $a.png
+	rm $a  
+done
